664fab2e53ef09e3a334d80454db6847f39c42fe
AymanTahmid <rownokshah02@gmail.com>
2025-06-03 23:30:43 +0600

    feature select

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import pickle
 
 data = pd.read_csv("heart.csv")
 print(data.head())
 print(data.isnull().sum())
+features = data[["Age", "Chest pain type", "BP", "Cholesterol", "Max HR", "ST depression", "Number of vessels fluro", "Thallium"]]
+target = data['Heart Disease']
+
+print(features)
+print(target)
