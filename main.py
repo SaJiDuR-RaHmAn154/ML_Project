ded7aa71842451483be39daf12e6a8ebf8696d68
“SaJiDuR-RaHmAn154” <sajidurrahmantarafder@gmail.com>
2025-06-08 10:31:48 +0600

    Updated the csv file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pickle
 
-data = pd.read_csv("heart.csv")
+data = pd.read_csv("Heart_Disease_Prediction.csv")
 print(data.head())
 print(data.isnull().sum())
 features = data[["Age", "Chest pain type", "BP", "Cholesterol", "Max HR", "ST depression", "Number of vessels fluro", "Thallium"]]
